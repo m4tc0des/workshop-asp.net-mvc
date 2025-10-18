5e1f49d1685af59be831ebd2c4b8d17ae386c5b6
m4tc0des <matt_ufo2@hotmail.com>
2025-10-18 13:11:08 -0300

    CRUD SCAFFOLDING 1.0

---
+++ SalesWebMvc/Models/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace SalesWebMvc.Models.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Department()
+        {
+        }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
