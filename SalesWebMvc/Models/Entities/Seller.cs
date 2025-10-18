451d02835353b74e371989e692d5d4daf0d0bab9
m4tc0des <matt_ufo2@hotmail.com>
2025-10-18 13:11:08 -0300

    CRUD SCAFFOLDING

---
+++ SalesWebMvc/Models/Entities/Seller.cs
@@ -0,0 +1,25 @@
+﻿namespace SalesWebMvc.Models.Entities
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public int DepartmentId { get; set; }
+        public Seller()
+        {
+        }
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+    }
+}
